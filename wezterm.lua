d7c7900bd366b3620892695e3d5ddc8806ca43da
seesaw-monster <L0Flies022@gmail.com>
2024-07-04 19:07:20 +0900

    表記方法の変更

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,21 @@
 local wezterm = require 'wezterm';
 
-return {
-    font = wezterm.font("DroidSansMono Nerd Font"),
-    use_ime = true,
-    font_size = 17.0,
-    color_scheme = "OneHalfDark", -- https://wezfurlong.org/wezterm/colorschemes/index.html
-    hide_tab_bar_if_only_one_tab = true,
-    adjust_window_size_when_changing_font_size = false,
-    force_reverse_video_cursor = true,
-}
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- 
+config.font = wezterm.font("DroidSansMono Nerd Font")
+config.font_size = 17.0
+config.use_ime = true
+
+-- https://wezfurlong.org/wezterm/colorschemes/index.html
+config.color_scheme = "OneHalfDark"
+config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
+config.force_reverse_video_cursor = true
+
+
+return config
