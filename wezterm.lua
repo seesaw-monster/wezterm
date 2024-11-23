aa6b221634f37d52ddc95d05f5f95c1d4871c073
seesaw-monster <seesawmonster777@gmail.com>
2024-11-23 17:36:31 +0900

    フォントの修正

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,8 @@ if wezterm.config_builder then
 end
 
 -- Visual #########################################################################
-config.font = wezterm.font(wezterm.target_triple == 'x86_64-pc-windows-msvc' and "DroidSansM Nerd Font" or "DroidSansMono Nerd Font")
+-- config.font = wezterm.font(wezterm.target_triple == 'x86_64-pc-windows-msvc' and "DroidSansM Nerd Font" or "DroidSansMono Nerd Font")
+config.font = wezterm.font("DroidSansM Nerd Font")
 config.font_size = 17.0
 config.use_ime = true
 
