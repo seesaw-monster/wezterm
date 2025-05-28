3d6215b45d3b786d6c2058d3e7442278312345a3
seesaw-monster <seesawmonster777@gmail.com>
2025-05-28 13:58:08 +0900

    FIX: remove <C-U> keybinds

--- keybinds.lua
+++ keybinds.lua
@@ -81,8 +81,6 @@ return {
     { key = 'R', mods = 'SHIFT|CTRL', action = act.ReloadConfiguration },
     { key = 'T', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
     { key = 'T', mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
-    { key = 'U', mods = 'CTRL', action = act.CharSelect{ copy_on_select = true, copy_to =  'ClipboardAndPrimarySelection' } },
-    { key = 'U', mods = 'SHIFT|CTRL', action = act.CharSelect{ copy_on_select = true, copy_to =  'ClipboardAndPrimarySelection' } },
     { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
     { key = 'V', mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
     { key = 'W', mods = 'CTRL', action = act.CloseCurrentTab{ confirm = true } },
